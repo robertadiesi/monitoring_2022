22762954ab16bc259b285a607003e5d568217469
Roberta Diesi <92572831+robertadiesi@users.noreply.github.com>
2021-10-15 10:59:30 +0200

    Create R_first_code.r

---
+++ R_first_code.r
@@ -0,0 +1,8 @@
+# This is my first code in github!
+
+# Here are the input data
+# Costanza data on streams
+water <- c (100, 200, 300, 400, 500)
+
+# Marta data on fish genomes
+fish <- c (10, 50, 60, 100, 200)
